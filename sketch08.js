ab3a68353da36b42a97bd6ee4340498448d008bf
micuat <micuat@gmail.com>
2018-02-24 22:07:04 -0500

    08 edit

--- sketch08.js
+++ sketch08.js
@@ -34,24 +34,39 @@ var s = function (sketch) {
 
     sketch.noStroke();
     sketch.fill(255);
-    sketch.ellipse(0, 0, 150);
-    if(ti >= 2) {
+    // sketch.ellipse(0, 0, 150);
+    let n = sketch.sin(sketch.millis() * 0.00025 * sketch.TWO_PI);
+    n = sketch.floor(sketch.map(n, -1, 1, 10, 70));
+    for (let i = 0; i < n; i++) {
+      sketch.fill(sketch.map(i, 0, n, 0, 255));
+      sketch.ellipse(0, 0, 200 - i);
+    }
+    if (ti >= 2) {
       sketch.blendMode(sketch.MULTIPLY);
       let r = 200;
       let g = 0;
       let b = 0;
       let te = t % 2;
-      if(te > 1.5) {
-        r = sketch.map(te, 1.5, 2, 200, 255);
-        g = b = sketch.map(te, 1.5, 2, 0, 255);
+      if (te < 0.75) {
+        r = sketch.map(te, 0, 0.75, 255, 200);
+        g = b = sketch.map(te, 0, 0.75, 255, 0);
+      }
+      if (te > 1.25) {
+        r = sketch.map(te, 1.25, 2, 200, 255);
+        g = b = sketch.map(te, 1.25, 2, 0, 255);
       }
       sketch.fill(r, g, b);
 
       sketch.push();
-      te = sketch.map(te, 0, 1, 150, 10);
-      te = sketch.constrain(te, 10, 150);
+      // te = sketch.map(te, 0, 0.5, 100, 10);
+      // te = sketch.constrain(te, 10, 100);
+      te = 10;
       sketch.rotate(t * 4 * sketch.TWO_PI % sketch.TWO_PI);
-      sketch.ellipse(te, 0, 150);
+      sketch.translate(te, 0);
+      for (let i = 0; i < sketch.map(r, 200, 255, 10, 0); i++) {
+        sketch.fill(255, 200, 200);
+        sketch.ellipse(0, 0, 200 - i * 5);
+      }
       sketch.pop();
 
       sketch.rect(0, 200, 100, 100);
